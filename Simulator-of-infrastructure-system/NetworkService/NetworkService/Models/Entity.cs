4fa6e3921c50193007f7151df187675a60b183ce
Tomasevic Nikola <tomasevic444@gmail.com>
2024-06-01 18:17:29 +0200

    + entity model

---
+++ Simulator-of-infrastructure-system/NetworkService/NetworkService/Models/Entity.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Models
+{
+    public class Entity : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        int id;
+        string name;
+        EntityType entityType;
+        int _value;
+
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                if (id != value)
+                {
+                    id = value;
+                    OnPropertyChanged(nameof(ID));
+                }
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+        public EntityType EntityType
+        {
+            get
+            {
+                return entityType;
+            }
+            set
+            {
+                if (entityType != value)
+                {
+                    entityType = value;
+                    OnPropertyChanged(nameof(EntityType));
+                }
+            }
+        }
+        public int Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if (_value != value)
+                {
+                    _value = value;
+                    OnPropertyChanged(nameof(Value));
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ Simulator-of-infrastructure-system/NetworkService/NetworkService/Models/EntityType.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Models
+{
+    public class EntityType : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        string type;
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                if (type != value)
+                {
+                    type = value;
+                    OnPropertyChanged("Type");
+                }
+            }
+        }
+        public EntityType(string type)
+        {
+            this.type = type;
+        }
+
+        public EntityType()
+        {
+
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
